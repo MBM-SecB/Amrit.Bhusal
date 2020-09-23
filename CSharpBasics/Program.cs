a99db64d94aa30db8c4ebe254d1f91a50721fc6e
Amrit2055 <mrtbhusal@gmail.com>
2020-09-23 10:58:48 +0545

    Learn FileIO

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -3,10 +3,8 @@ class Program
 {
     static void Main()
     {
-        var square1=new Square(23.45);
-        var square2=new Square(4);
-        var rectangle = new Rectangle(23.5,12.2);
-        rectangle.PrintResults();
+        FileIO file = new FileIO();
+        file.LearnDirectory();
         
     }
       
