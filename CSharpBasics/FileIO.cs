b86c99c2a861221699c7b299b3c8d3be4aa2e966
Amrit2055 <mrtbhusal@gmail.com>
2020-09-24 10:15:34 +0545

    FIle Handling

--- CSharpBasics/FileIO.cs
+++ CSharpBasics/FileIO.cs
@@ -48,18 +48,4 @@ public class FileIO
         }
         
     }
-
-    public void Assignment()
-    {
-        string rootFolderName = "Nepal";
-        Directory.CreateDirectory(rootFolderName);
-        string[] subFoldersName = {"Nepal","Korea","China","Russia","Pakistan","Bangladesh","Bhutan","Srilanka","Japan","Australia"};
-        foreach(string sub in subFoldersName)
-        {
-            string subFolderNamePath = rootFolderName + "/" + sub;
-            Directory.CreateDirectory(subFolderNamePath);
-            string filePath = subFolderNamePath + "/" + "Test.txt";
-            File.WriteAllText(filePath,sub);
-        }
-    }
 }
--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main()
     {
         FileIO file = new FileIO();
-        file.Assignment();
+        file.LearnDirectory();
         
     }
       
