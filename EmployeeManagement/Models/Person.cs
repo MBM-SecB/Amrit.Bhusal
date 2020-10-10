fda357129de3515a234704ae7e4aee8c03affba6
Amrit2055 <mrtbhusal@gmail.com>
2020-10-10 18:14:11 +0545

    Created tiny Em app

---
+++ EmployeeManagement/Models/Person.cs
@@ -0,0 +1,10 @@
+public class Person
+{
+    public string FirstName {get; set;}
+    public string Surname {get; set;}
+    public string Address {get; set;}
+    public char Gender {get; set;} = 'M';
+    public double Salary {get; set;}
+
+
+}
