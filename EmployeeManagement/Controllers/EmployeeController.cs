abcbbeb9d76ad5a0ccb7d66885492740a91651bb
Amrit2055 <mrtbhusal@gmail.com>
2020-10-12 13:29:41 +0545

    Added Employee Details

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,28 +1,20 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        
-
-        //Object initializer syntax
-        Person empl1 = new Person()
-        {
-            FirstName= "Sekhar",
-            Surname= "Khanal",
-            Address = "Sankhamul",
-            Salary= 10000.0
-
-        };
-        Person empl2 = new Person{FirstName= "Rachan",Surname= "Nepal",Address = "Ktm",Salary= 15000};
-        Person empl3 = new Person{FirstName= "Raman",Surname= "Shiwakoti",Address = "Koteshwor",Salary= 25000};
-        Person empl4 = new Person{FirstName= "Nischal",Surname= "Karki",Address = "bhaktapur",Salary= 5000};
-        Person empl5 = new Person{FirstName= "Sagar",Surname= "Neupane",Address = "Jumla",Salary= 35000};
-        Person empl6 = new Person{FirstName= "Raj",Surname= "Tiwari",Address = "KOteshwor",Salary= 20000};
-
-        List<Person> employees = new List<Person>() {empl1, empl2,empl3,empl4,empl5,empl6};
+        List<Person> employees = Person.GetPerson();
         return View(employees);
+    }
+    public ActionResult Detail(string firstName)
+    {
+        List<Person> employees =Person.GetPerson();
+        var employee = employees.Where(x =>x.FirstName == firstName).First();
+        return View(employee);
+        
+        
     }
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,3 +1,6 @@
+
+using System.Collections.Generic;
+
 public class Person
 {
     public string FirstName {get; set;}
@@ -5,6 +8,26 @@ public class Person
     public string Address {get; set;}
     public char Gender {get; set;} = 'M';
     public double Salary {get; set;}
+
+    public static List<Person> GetPerson()
+    {
+           //Object initializer syntax
+        Person empl1 = new Person()
+        {
+            FirstName= "Sekhar",
+            Surname= "Khanal",
+            Address = "Sankhamul",
+            Salary= 10000.0
+
+        };
+        Person empl2 = new Person{FirstName= "Rachan",Surname= "Nepal",Address = "Ktm",Salary= 15000};
+        Person empl3 = new Person{FirstName= "Raman",Surname= "Shiwakoti",Address = "Koteshwor",Salary= 25000};
+        Person empl4 = new Person{FirstName= "Nischal",Surname= "Karki",Address = "bhaktapur",Salary= 5000};
+        Person empl5 = new Person{FirstName= "Sagar",Surname= "Neupane",Address = "Jumla",Salary= 35000};
+        Person empl6 = new Person{FirstName= "Raj",Surname= "Tiwari",Address = "KOteshwor",Salary= 20000};
+
+        return new List<Person>() {empl1, empl2,empl3,empl4,empl5,empl6};
+    }
 
 
 }
