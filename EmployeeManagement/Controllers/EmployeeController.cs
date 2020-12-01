5a33d179cf1b65821e61f742ce00f6b4fa74abf2
Amrit2055 <mrtbhusal@gmail.com>
2020-12-01 19:41:38 +0545

    Querying database

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -4,19 +4,24 @@ using Microsoft.AspNetCore.Mvc;
 using EmployeeManagement.Data;
 
 public class EmployeeController : Controller
+
 {
+    private EMSContext db;
+    public EmployeeController(EMSContext _db)
+    {
+        db =_db;
+
+    }
     public ActionResult Index()
     {
         //List<Person> employees = Person.GetPerson();
-        var db = new EMSContext();
-        var employees = db.Employees.Select(x => x.Gender == 'm');
+       var employees =db.Employees.ToList();
         
         return View(employees);
     }
-    public ActionResult Detail(string firstName)
+    public ActionResult Detail(int id)
     {
-        List<Person> employees =Person.GetPerson();
-        var employee = employees.Where(x =>x.FirstName == firstName).First();
+        var employee=db.Employees.Find(id);
         return View(employee);
         
         
@@ -25,10 +30,31 @@ public class EmployeeController : Controller
     {
         return View();
     }
+    public ActionResult Delete()
+    {
+        return View();
+    }
     [HttpPost]
     public ActionResult<bool> Add(Person person)
     {
-        return true;
+        db.Employees.Add(person);
+        db.SaveChanges();
+
+        //var rowsAffected=db.SaveChanges();
+        return RedirectToAction(nameof(Index));
+
+
+    }
+
+    [HttpPost]
+    public ActionResult Delete(int id)
+    {
+        var person=db.Employees.Find(id);
+        db.Employees.Remove(person);
+        db.SaveChanges();
+
+        //var rowsAffected=db.SaveChanges();
+        return RedirectToAction(nameof(Index));
 
 
     }
